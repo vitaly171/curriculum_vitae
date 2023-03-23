30f130614ca79c5d3ba61d02f3b8e619d8e44a95
vitaly171 <72026466+vitaly171@users.noreply.github.com>
2023-03-24 00:11:28 +0300

    header

--- my-app/src/components/Container/Container.jsx
+++ my-app/src/components/Container/Container.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import Header from "../Header";
 import s from "./Container.module.css";
 
 export default function Container() {
-  return <div className={s.container}></div>;
+  return (
+    <div className={s.container}>
+      <Header />
+    </div>
+  );
 }
---
+++ my-app/src/components/Header/Header.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import s from "./Header.module.css";
+
+export default function Header() {
+  return <div className={s.header__container}>hello</div>;
+}
