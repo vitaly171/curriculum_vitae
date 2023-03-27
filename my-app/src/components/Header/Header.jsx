b731f15c5cb513e4285f2903a510361eb4f3ffbe
vitaly171 <72026466+vitaly171@users.noreply.github.com>
2023-03-27 22:51:49 +0300

    header

--- my-app/src/components/Header/Header.jsx
+++ my-app/src/components/Header/Header.jsx
@@ -2,5 +2,31 @@ import React from "react";
 import s from "./Header.module.css";
 
 export default function Header() {
-  return <div className={s.header__container}>hello</div>;
+  return (
+    <div className={s.header__container}>
+      <a href="/">Logo</a>
+      <nav className={s.header__nav}>
+        <ul className={s.header__list}>
+          <li className={s.header__item}>
+            <a href="/">About Me</a>
+          </li>
+          <li className={s.header__item}>
+            <a href="/">EDUCATION</a>
+          </li>
+          <li className={s.header__item}>
+            <a href="/">EXPERIENCE</a>
+          </li>
+          <li className={s.header__item}>
+            <a href="/">TECH SKILLS</a>
+          </li>
+          <li className={s.header__item}>
+            <a href="/">HOBBIES</a>
+          </li>
+          <li className={s.header__item}>
+            <a href="/">LANGUAGE SKILLS</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
